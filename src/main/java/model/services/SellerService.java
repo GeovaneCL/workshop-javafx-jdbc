a8610a87f695d3d4c5bf295f599d9e0795003afa
Geovane Caixeta <caixetageovane@gmail.com>
2026-08-09 14:28:40 -0300

    SellerList

---
+++ src/main/java/model/services/SellerService.java
@@ -0,0 +1,28 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.dao.FactoryDao;
+import model.entities.Seller;
+
+public class SellerService {
+    
+private SellerDao dao = FactoryDao.createSellerDao();
+
+    public List<Seller> findAll(){
+        return dao.findAll();
+    }
+
+    public void saveOrUpdate(Seller obj){
+        if( obj.getId()== null){
+            dao.insert(obj);
+        }
+        else {
+            dao.update(obj);
+        }
+    }
+    public void remove(Seller obj){
+        dao.deleteById(obj.getId());
+    }
+}
